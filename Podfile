a9cf1e7c5682dd32754092cf4c4757707a4a4666
Edward Suczewski <ed.suczewski@gmail.com>
2017-02-23 09:02:04 -0500

    Added Firebase Auth & Database
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- Podfile
+++ Podfile
@@ -6,5 +6,8 @@ target 'QuickDoc' do
   use_frameworks!
 
   # Pods for QuickDoc
+  pod ‘Firebase/Core’
+  pod ‘Firebase/Auth’
+  pod 'Firebase/Database'
 
 end
--- QuickDoc/AppDelegate.swift
+++ QuickDoc/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
@@ -34,7 +35,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if FIRAuth.auth()?.currentUser == nil {
+            FIRAuth.auth()?.signInAnonymously(completion: nil)
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
