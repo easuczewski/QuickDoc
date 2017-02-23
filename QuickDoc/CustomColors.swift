224a611b9bd5faf39baabe67b9c96f6e750f9f38
Edward Suczewski <ed.suczewski@gmail.com>
2017-02-23 09:54:19 -0500

    Finished LaunchScreen
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- QuickDoc/CustomColors.swift
+++ QuickDoc/CustomColors.swift
@@ -11,8 +11,8 @@ import UIKit
 
 class CustomColor {
     
-    static let quickDocBlue = UIColor(colorLiteralRed: 69/255, green: 93/255, blue: 140/255, alpha: 1.0)
+    static let quickDocBlue = UIColor(colorLiteralRed: 34/255, green: 57/255, blue: 100/255, alpha: 1.0)
     
-    static let quickDocRed = UIColor(colorLiteralRed: 215/255, green: 38/255, blue: 59/255, alpha: 1.0)
+    static let quickDocRed = UIColor(colorLiteralRed: 215/255, green: 38/255, blue: 56/255, alpha: 1.0)
     
 }
